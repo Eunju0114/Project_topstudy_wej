38b17cb648623491dfce1c1919a1bd2f0926ad5b
Eunju0114 <wangeunju0114@gmail.com>
2022-08-10 19:54:07 +0900

    ArticleService에 있던 단순데이터 저장광 관련된 로직은 ArticleRepository으로 옮김

--- src/main/java/com/wej/exam/demo/service/ArticleService.java
+++ src/main/java/com/wej/exam/demo/service/ArticleService.java
@@ -1,73 +1,40 @@
 package com.wej.exam.demo.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.wej.exam.demo.reporsitory.ArticleRepository;
 import com.wej.exam.demo.vo.Article;
 
 @Service
 public class ArticleService {
 
-	private int articlesLastId;
-	private List<Article> articles;
+	private ArticleRepository articleRepository;
 
-	public ArticleService() {
-		articlesLastId = 0;
-		articles = new ArrayList<>();
-
-		makeTestData();
-	}
-
-	public void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-
-			writeArticle(title, body);
-		}
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+		articleRepository.makeTestData();
 	}
 
 	public Article writeArticle(String title, String body) {
-		int id = articlesLastId + 1;
-		Article article = new Article(id, title, body);
-
-		articles.add(article);
-		articlesLastId = id;
-
-		return article;
+		return articleRepository.writeArticle(title, body);
 	}
 
 	public Article getArticle(int id) {
-
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-
-			}
-		}
-		return null;
-
+		return articleRepository.getArticle(id);
 	}
 
 	public List<Article> getArticles() {
-		return articles;
+		return articleRepository.getArticles();
 	}
 
 	public void deleteArticle(int id) {
-
-		Article article = getArticle(id);
-
-		articles.remove(article);
+		articleRepository.deleteArticle(id);
 	}
 
 	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-
-		article.setTitle(title);
-		article.setBody(body);
-
+		articleRepository.modifyArticle(id, title, body);
 	}
 
 }
