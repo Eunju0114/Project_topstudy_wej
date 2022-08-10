e556aaf499e02f11e9b9e1583b04edf3b9d22c67
Eunju0114 <wangeunju0114@gmail.com>
2022-08-10 19:26:54 +0900

    게시물 삭제 수정 상세보기 액션 구현

--- src/main/java/com/wej/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/wej/exam/demo/controller/UsrArticleController.java
@@ -21,7 +21,20 @@ public class UsrArticleController {
 	public Article doAdd(String title, String body) {
 
 		Article article = articleService.writeArticle(title, body);
-		
+
+		return article;
+	}
+
+	@RequestMapping("usr/article/getArticle")
+	@ResponseBody
+	public Object getArticle(int id) {
+
+		Article article = articleService.getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+
 		return article;
 	}
 
@@ -29,5 +42,34 @@ public class UsrArticleController {
 	@ResponseBody
 	public List<Article> getArticles() {
 		return articleService.getArticles();
+	}
+
+	@RequestMapping("usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+
+		Article article = articleService.getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+
+		articleService.deleteArticle(id);
+
+		return id + "번 게시물을 삭제하였습니다.";
+	}
+
+	@RequestMapping("usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = articleService.getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+
+		articleService.modifyArticle(id, title, body);
+
+		return id + "번 게시물을 수정하였습니다.";
 	}
 }
