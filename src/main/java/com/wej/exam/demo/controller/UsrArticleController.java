2568b6ee936fc98684c58ace065fd9f48845f0fb
Eunju0114 <wangeunju0114@gmail.com>
2022-08-18 16:42:18 +0900

    게시물 수정시 권한 체크

--- src/main/java/com/wej/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/wej/exam/demo/controller/UsrArticleController.java
@@ -86,14 +86,15 @@ public class UsrArticleController {
 
 		Article article = articleService.getArticle(id);
 		
-		if(article.getMemberId() != loginedMemberId) {
-			return "권한이 없습니다.";
-		}
-
 		if (article == null) {
 			return id + "번 게시물이 존재하지 않습니다.";
 		}
 
+		
+		if(article.getMemberId() != loginedMemberId) {
+			return "권한이 없습니다.";
+		}
+		
 		articleService.deleteArticle(id);
 
 		return id + "번 게시물을 삭제하였습니다.";
@@ -101,11 +102,28 @@ public class UsrArticleController {
 
 	@RequestMapping("usr/article/doModify")
 	@ResponseBody
-	public String doModify(int id, String title, String body) {
-		Article article = articleService.getArticle(id);
+	public String doModify(HttpSession httpSession, int id, String title, String body) {
+		
+		boolean isLogined = false;
+		int loginedMemberId = 0;
 
+		if ( httpSession.getAttribute("loginedMemberId") != null ) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+		
+		if( isLogined == false ) {
+			return "로그인 후 이용해주세요.";
+		}
+
+		Article article = articleService.getArticle(id);
+		
 		if (article == null) {
 			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		
+		if(article.getMemberId() != loginedMemberId) {
+			return "권한이 없습니다.";
 		}
 
 		articleService.modifyArticle(id, title, body);
