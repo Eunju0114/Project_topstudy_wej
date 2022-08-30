6c43c281a3f453c10f03af4e6648acc565fd1297
Eunju0114 <wangeunju0114@gmail.com>
2022-08-30 18:48:26 +0900

    데이지 UI 적용

--- src/main/java/com/wej/exam/demo/vo/Article.java
+++ src/main/java/com/wej/exam/demo/vo/Article.java
@@ -15,8 +15,17 @@ public class Article {
 	private String regDate;
 	private String updateDate;
 	private int memberId;
-	
+
 	private String extra_writerName;
 	private boolean extra_actorCanDelete;
-	
+	private boolean extra_actorCanModify;
+
+	public String getRegDateForPrint() {
+		return regDate.substring(2, 16);
+	}
+
+	public String getUpdateDateForPrint() {
+		return updateDate.substring(2, 16);
+	}
+
 }
